c5774751221a9a741ba6274928ff0cdaf70447e6
Yasunari_Kainuna <yasu6491@icloud.com>
2021-07-19 16:41:39 +0900

    fix:capybara修正・Chromeインストール

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,13 +1,13 @@
-require 'capybara/rspec'
-
 RSpec.configure do |config|
+  Capybara.default_max_wait_time = 2
+
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :headless_chrome, options: {
-      browser: :remote,
-      url: ENV.fetch('SELENIUM_DRIVER_URL'),
-      desired_capabilities: :chrome
-    }
-    Capybara.server_host = 'web'
-    Capybara.app_host = 'http://web'
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 900] do |option|
+      option.add_argument 'no-sandbox'
+    end
   end
 end
